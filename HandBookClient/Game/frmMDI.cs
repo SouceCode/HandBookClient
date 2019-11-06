7af9c2dbc74504ea3257cfbcddc6e12ca53b899e
爱悠的沫夏 <oncwnuMRLtgs-1vuQSn30RxDTltI@git.weixin.qq.com>
2019-11-06 16:58:59 +0800

    由于改用了Splitcontainer控件拆分工具影响到了先前的避免重复打开窗体的打开， 现新增方法 IsExistsplitContainer1Panel2Controls用于判断splitContainer1控件Panel2里的是否有窗体等； 另外为了避免出现无法将顶级控件添加到控件。的错误，给新增的窗体配置属性TopLevel = false;

--- HandBookClient/Game/frmMDI.cs
+++ HandBookClient/Game/frmMDI.cs
@@ -26,10 +26,11 @@ namespace HandBookClient.Game
 
         private void menuItemGameSetting_Click(object sender, EventArgs e)
         {
-            if (IsExistMDI("frmGameSettingList")) { return; }   //防止重复打开窗体，新增如下代码
+            if (IsExistMDI("frmGameSettingList")|| IsExistsplitContainer1Panel2Controls("frmGameSettingList")) { return; }   //防止重复打开窗体，新增如下代码
             Game.frmGameSettingList frmGameSetting = new Game.frmGameSettingList();
             frmGameSetting.MdiParent = this;
             frmGameSetting.WindowState = FormWindowState.Maximized;//使MDI子窗体一打开就最大化
+            frmGameSetting.TopLevel = false;
             frmGameSetting.Dock = DockStyle.Fill;
             splitContainer1.Panel2.Controls.Add(frmGameSetting);
             frmGameSetting.Show();
@@ -42,8 +43,7 @@ namespace HandBookClient.Game
         /// <param name="frmName"></param>
         /// <returns></returns>
         public bool IsExistMDI(string frmName)
-        {
-
+        {            
             foreach (Form childForm in this.MdiChildren)
             {//检查是不是要打开的窗体
                 if (childForm.Name == frmName)
@@ -56,7 +56,27 @@ namespace HandBookClient.Game
             }
             return false;
         }
+        /// <summary>
+        /// 判断是否有重复的窗体
+        /// </summary>
+        /// <param name="frmName"></param>
+        /// <returns></returns>
+        public bool IsExistsplitContainer1Panel2Controls(string frmName)
+        {
+            
 
+            foreach (Form childForm in this.splitContainer1.Panel2.Controls)
+            {//检查是不是要打开的窗体
+                if (childForm.Name == frmName)
+                {//是的话，则显示
+                    childForm.Visible = true;
+                    //是的话，并激活该窗体
+                    childForm.Activate();
+                    return true;
+                }
+            }
+            return false;
+        }
         private void frmMDI_FormClosed(object sender, FormClosedEventArgs e)
         {
         }
@@ -65,7 +85,8 @@ namespace HandBookClient.Game
         {
             //在主窗口打开的时候，就打开子窗口
             frmChild.MdiParent = this;
-            frmChild.Location = new Point(0, 0);//显示位置
+            frmChild.TopLevel = false;
+            //frmChild.Location = new Point(0, 0);//显示位置
             frmChild.Dock = DockStyle.Fill;
             splitContainer1.Panel2.Controls.Add(frmChild);
             frmChild.Show();
